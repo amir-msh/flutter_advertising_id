367d7479b993b78ec3de1673877b1349f9318fae
Amir Mohammad Mashayekhi <mashayekhiamirmohammad@gmail.com>
2025-07-27 17:26:42 +0300

    fix: Remove conflicting allprojects repository configuration

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -1,15 +1,11 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+// The problematic 'allprojects' block has been removed.
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+// This part is fine and should remain. It configures the build output directory.
+val newBuildDir: org.gradle.api.file.Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir: org.gradle.api.file.Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
